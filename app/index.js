c1878039d174bd0d58c87dc3c3ab104c69639a1f
Zeke <ezekielbadowski@gmail.com>
2019-09-17 13:28:41 -0500

    config setup and checkout modeling

---
+++ app/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Dependencies
+const path = require('path');
+const router = require('express').Router(); // Create a Router instance
+
+// Require all API routes
+const checkout = require('./checkouts');
+const staff = require('./staff');
+
+// Require authentication routes
+const auth = require('./auth');
+
+
+// API routes
+router.use('/api/checkout', checkout.controller);
+router.use('/api/staff', staff.controller);
+
+// Authentication routes
+router.use('/auth/login', auth.login);
+router.use('/auth/session', auth.session);
+
+
+// If no API routes are hit, send React app
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Require and configure dotenv
+require('dotenv').config();
+
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+
+// App configuration
+const tgnv = require('./config');
+
+// Require all routes
+const routes = require('./app');
+
+// Initialize Express
+const app = express();
+
+// Define port
+const PORT = process.env.PORT || 3001;
+
+// Set up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files from React app
+app.use(express.static('client/build'));
+
+// Connect to MongoDB
+mongoose.connect(tgnv.config.mongodb.uri, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true
+});
+
+// Routes
+app.use(routes);
+
+// Start server
+app.listen(PORT);
